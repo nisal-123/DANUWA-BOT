e9b4569cc29d4c5aa49e3e30059b67f26db10166
nisal-123 <nisalmuhandiram2004@gmail.com>
2025-06-18 09:17:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ function convertToBool(text, fault = 'true') {
 
 
 const defaultConfig = {
-  SESSION_ID: "", // Put your session id here
+  SESSION_ID: "ydYVWLaZ#-PD-dWbqRiZDVbuAKnpjDsYKsjb1TlBupApo3vz4CL4", // Put your session id here
   ALIVE_IMG: "https://github.com/DANUWA-MD/DANUWA-BOT/blob/main/images/Danuwa%20-%20MD.png?raw=true",
   ALIVE_MSG: "*Hello👋 DANUWA-MD Is Alive Now😍*", // Change alive msg from here
   OPENAI_API_KEY: "",// Add OPENAI API key here
